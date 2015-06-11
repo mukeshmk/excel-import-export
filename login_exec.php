596cfd0b8a2be7aed41fcf794fa9e25f6c3f0534
Mukesh <amukesh.mk@gmail.com>
2015-06-11 18:25:50 +0530

    create login_exec.php file
    well have do that right ?

---
+++ login_exec.php
@@ -0,0 +1,30 @@
+<?php
+	//Start session
+	session_start();
+ 
+	//Include database connection details
+	require_once('connection.php');
+ 
+	//Array to store validation errors
+	$errmsg_arr = array();
+ 
+	//Validation error flag
+	$errflag = false;
+ 
+	//Function to sanitize values received from the form. Prevents SQL injection
+	function clean($str) 
+	{
+		$str = @trim($str);
+		if(get_magic_quotes_gpc()) 
+		{
+			$str = stripslashes($str);
+		}
+		return mysql_real_escape_string($str);
+	}
+ 
+	//Sanitize the POST values
+	$username = clean($_POST['username']);
+	$password = clean($_POST['password']);
+ 
+	
+?>
