d0bfe0af9fefc3e7b3ee317f232d2b06ab1ca55a
Mukesh <amukesh.mk@gmail.com>
2015-06-11 18:32:08 +0530

    home page
    finally something done need to more

---
+++ home.php
@@ -0,0 +1,24 @@
+<?php
+	require_once('auth.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<style type="text/css">
+<!--
+.style1 {
+	font-size: 36px;
+	font-weight: bold;
+}
+-->
+</style>
+</head>
+ 
+<body>
+<p align="center" class="style1">Login successfully </p>
+<p align="center">This page is the home, you can put some stuff here......</p>
+<p align="center"><a href="index.php">logout</a></p>
+</body>
+</html>
