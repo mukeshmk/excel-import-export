84532a79d714453915f55bebc33d0b47a8affdea
Mukesh <amukesh.mk@gmail.com>
2015-06-11 18:22:33 +0530

    back check
    To disallow the user to go back as if it still logged in.

--- Index.php
+++ Index.php
@@ -13,4 +13,36 @@
 <h1 style="color:blue">My Intern</h1>
 
 </body>
+
+<form name="loginform" action="login_exec.php" method="post">
+<table width="309" border="0" align="center" cellpadding="2" cellspacing="5">
+  <tr>
+    <td colspan="2">
+		<!--the code bellow is used to display the message of the input validation-->
+		 <?php
+			if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count($_SESSION['ERRMSG_ARR']) >0 ) {
+			echo '<ul class="err">';
+			foreach($_SESSION['ERRMSG_ARR'] as $msg) {
+				echo '<li>',$msg,'</li>'; 
+				}
+			echo '</ul>';
+			unset($_SESSION['ERRMSG_ARR']);
+			}
+		?>
+	</td>
+  </tr>
+  <tr>
+    <td width="116"><div align="right">Username</div></td>
+    <td width="177"><input name="username" type="text" /></td>
+  </tr>
+  <tr>
+    <td><div align="right">Password</div></td>
+    <td><input name="password" type="text" /></td>
+  </tr>
+  <tr>
+    <td><div align="right"></div></td>
+    <td><input name="" type="submit" value="login" /></td>
+  </tr>
+</table>
+</form>
 </html>
