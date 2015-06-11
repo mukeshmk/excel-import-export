9d1d83f4d9c7453f4729d7924dc56bb8c0b10597
Mukesh <amukesh.mk@gmail.com>
2015-06-11 18:20:44 +0530

    starting off
    clearing the var to be used

--- Index.php
+++ Index.php
@@ -1,4 +1,12 @@
-<!DOCTYPE html>
+<?php
+	//Start session
+	session_start();	
+	//Unset the variables stored in session
+	unset($_SESSION['SESS_MEMBER_ID']);
+	unset($_SESSION['SESS_FIRST_NAME']);
+	unset($_SESSION['SESS_LAST_NAME']);
+?>
+
 <html>
 <body>
 
