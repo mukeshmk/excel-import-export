13388734b801dcaa82fea22ed4c2180ea6c0bae5
Mukesh <amukesh.mk@gmail.com>
2015-06-12 23:25:25 +0530

    Change in look
    Change in look of input page

--- input_page.php
+++ input_page.php
@@ -9,23 +9,20 @@
 	</p>
 	<h1 align="center">Fill it up man</h1>
     <body>
-        <form action="ip_php.php" method="post" autocomplete="off">
+        <form action="p2.php" method="post" autocomplete="off">
+			
+			<table style="width:0%" align="center">
+			<tr>
+				
+			<td>
 			<table style="width:0%" align="center">
 				<tr>
 					<td>S.No:-</td>						
 					<td><input type="text" name="sno" required></td>
 				</tr>
 				<tr>
-					<td>Seat No:-</td>
-					<td><input type="text" name="SeatNo" required></td>
-				</tr>
-				<tr>
 					<td>Employee ID:-</td>
 					<td><input type="text" name="EmpID" required></td>
-				</tr>
-				<tr>
-					<td>Employee Name:-</td>
-					<td><input type="text" name="EmpNm" required></td>
 				</tr>
 				<tr>
 					<td>Location:-</td>
@@ -40,10 +37,6 @@
 					</td>
 				</tr>
 				<tr>
-					<td>Floor/Block:-</td>						
-					<td><input type="text" name="fb" required></td>
-				</tr>
-				<tr>
 					<td>Status:-</td>
 					<td>
 						<select name="status" required>
@@ -56,12 +49,67 @@
 					</td>
 				</tr>
 				<tr>
-					<td>Asset Tag No:-</td>						
-					<td><input type="text" name="atn" required></td>
-				</tr>
-				<tr>
 					<td>Host Name:-</td>						
 					<td><input type="text" name="HostNm" required></td>
+				</tr>
+				<tr>
+					<td>Brand:-</td>						
+					<td><input type="text" name="Brand" required></td>
+				</tr>
+				<tr>
+					<td>Serial No:-</td>						
+					<td><input type="text" name="SrlNo" required></td>
+				</tr>
+				<tr>
+					<td>Associated Devices S.No:-</td>						
+					<td><input type="text" name="ADSNo" required></td>
+				</tr>
+				<tr>
+					<td>Memory:-</td>						
+					<td><input type="text" name="Mem" required></td>
+				</tr>
+				<tr>
+					<td>Operating System:-</td>						
+					<td><input type="text" name="OS" required></td>
+				</tr>
+				<tr>
+					<td>Invoice Details:-</td>						
+					<td><input type="text" name="Inv" required></td>
+				</tr>
+				<tr>
+					<td>Warrenty Expiry:-</td>						
+					<td><input type="date" name="Wrty" required></td>
+				</tr>
+				<tr>
+					<td>Contact No:-</td>						
+					<td><input type="text" name="CNo" required></td>
+				</tr>	
+				<tr>
+					<td>Remarks:-</td>						
+					<td><input type="text" name="Rmks" required></td>
+				</tr>
+			</table>
+			</td>
+				
+			<td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+				
+			<td>
+			<table style="width:0%" align="right">
+				<tr>
+					<td>Seat No:-</td>
+					<td><input type="text" name="SeatNo" required></td>
+				</tr>
+				<tr>
+					<td>Employee Name:-</td>
+					<td><input type="text" name="EmpNm" required></td>
+				</tr>
+				<tr>
+					<td>Floor/Block:-</td>						
+					<td><input type="text" name="fb" required></td>
+				</tr>
+				<tr>
+					<td>Asset Tag No:-</td>						
+					<td><input type="text" name="atn" required></td>
 				</tr>
 				<tr>
 					<td>Asset Type:-</td>
@@ -84,16 +132,8 @@
 					</td>
 				</tr>
 				<tr>
-					<td>Brand:-</td>						
-					<td><input type="text" name="Brand" required></td>
-				</tr>
-				<tr>
 					<td>Model:-</td>						
 					<td><input type="text" name="Model" required></td>
-				</tr>
-				<tr>
-					<td>Serial No:-</td>						
-					<td><input type="text" name="SrlNo" required></td>
 				</tr>
 				<tr>
 					<td>Associtated Devices:-</td>
@@ -108,61 +148,35 @@
 					</td>
 				</tr>
 				<tr>
-					<td>Associated Devices S.No:-</td>						
-					<td><input type="text" name="ADSNo" required></td>
-				</tr>
-				<tr>
 					<td>Hard Disk:-</td>						
 					<td><input type="text" name="HDD" required></td>
 				</tr>
-				<tr>
-					<td>Memory:-</td>						
-					<td><input type="text" name="Mem" required></td>
-				</tr>
+				
 				<tr>
 					<td>Processor:-</td>						
 					<td><input type="text" name="Proc" required></td>
-				</tr>
-				<tr>
-					<td>Operating System:-</td>						
-					<td><input type="text" name="OS" required></td>
 				</tr>
 				<tr>
 					<td>Purchased On:-</td>						
 					<td><input type="date" name="PurchOn" required></td>
 				</tr>
 				<tr>
-					<td>Invoice Details:-</td>						
-					<td><input type="text" name="Inv" required></td>
-				</tr>
-				<tr>
 					<td>Vendor Details:-</td>						
 					<td><input type="text" name="Vend" required></td>
-				</tr>
-				<tr>
-					<td>Warrenty Expiry:-</td>						
-					<td><input type="date" name="Wrty" required></td>
 				</tr>
 				<tr>
 					<td>Date of Issue:-</td>						
 					<td><input type="date" name="DoI" required></td>
 				</tr>
 				<tr>
-					<td>Contact No:-</td>						
-					<td><input type="text" name="CNo" required></td>
-				</tr>
-				<tr>
 					<td>EMail ID:-</td>						
 					<td><input type="text" name="EMail" required></td>
 				</tr>
-				<tr>
-					<td>Remarks:-</td>						
-					<td><input type="text" name="Rmks" required></td>
-				</tr>
-				<tr>
-					<td><button type="submit" >Submit</button></td>
-				</tr>
 			</table>
+			</td>
+			</tr>
+			</table>
+			<p align="center"><button type="submit">Submit</button></p>
         </form>
     </body>
 </html>
