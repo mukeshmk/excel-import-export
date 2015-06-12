9ae7798653fb50960f11b41dec3aaff3f6c87754
Mukesh <amukesh.mk@gmail.com>
2015-06-12 20:15:03 +0530

    creating the variable for the table
    creating the variable for the table in the ip_php.php file

---
+++ ip_php.php
@@ -0,0 +1,32 @@
+<?php
+
+	$sno=$_POST['sno'];
+	$SeatNo=$_POST['SeatNo'];
+	$EmpID=$_POST['EmpID'];
+	$EmpNm=$_POST['EmpNm'];
+	$Loc=$_POST['Loc'];
+	$fb=$_POST['fb'];
+	$status=$_POST['status'];
+	$atn=$_POST['atn'];
+	$HostNm=$_POST['HostNm'];
+	$Asstyp=$_POST['Asstyp'];
+	$Brand=$_POST['Brand'];
+	$Model=$_POST['Model'];
+	$SrlNo=$_POST['SrlNo'];
+	$AssDev=$_POST['AssDev'];
+	$ADSNo=$_POST['ADSNo'];
+	$HDD=$_POST['HDD'];
+	$Mem=$_POST['Mem'];
+	$Proc=$_POST['Proc'];
+	$OS=$_POST['OS'];
+	$PurchOn=$_POST['PurchOn'];
+	$Inv=$_POST['Inv'];
+	$Vend=$_POST['Vend'];
+	$Wrty=$_POST['Wrty'];
+	$DoI=$_POST['DoI'];
+	$CNo=$_POST['CNo'];
+	$EMail=$_POST['EMail'];
+	$Rmks=$_POST['Rmks'];
+
+
+?>
