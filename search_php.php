2e87e8f5cc5a026670466c4a1eb2b2e218b2390b
Mukesh <amukesh.mk@gmail.com>
2015-06-13 18:45:22 +0530

    searched fileds
    displaying the searched fileds

--- search_php.php
+++ search_php.php
@@ -11,7 +11,20 @@
 		{
 			echo("<p>You didn't select any Fields!</p>\n");
 		} 
-		
+		else 
+		{
+			$nFields = count($aFields);
+			
+			echo("<p>You selected $nFields Fields: ");
+			for($i=0; $i < $nFields; $i++)
+			{
+				echo($aFields[$i] . " ");
+			}
+			echo("</p>");
+			
+			
+			
+		}
 	}
 ?>
 		
