aa253d81483b5c70ab3b7b092eafe845e407bdee
Mukesh <amukesh.mk@gmail.com>
2015-06-13 18:44:26 +0530

    No field in search
    No field in search case

--- search_php.php
+++ search_php.php
@@ -3,7 +3,16 @@
 	<body>
 		
 <?php
-	
+	if(isset($_POST['formSubmit'])) 
+	{
+		$aFields = $_POST['formFields'];
+		
+		if(!isset($aFields)) 
+		{
+			echo("<p>You didn't select any Fields!</p>\n");
+		} 
+		
+	}
 ?>
 		
 	</body>
