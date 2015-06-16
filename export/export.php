e64d00064ceb0f581bef6a7b38986ca0d2e81d30
Mukesh <amukesh.mk@gmail.com>
2015-06-16 14:01:06 +0530

    export complete
    can be export from any table display point.

--- export/export.php
+++ export/export.php
@@ -2,5 +2,30 @@
 	require_once('../login/auth.php');
 ?>
 <?php
-	echo($_SESSION['qur']);
+
+	$connect = mysqli_connect("localhost","root","");
+	$db = mysqli_select_db($connect,"my_intern");
+
+	$q=$_SESSION['qur'];
+	$qur=mysqli_query($connect,$q);
+
+	echo($q);
+	$filename = "export_file.xls"; // File Name
+
+	// Download file
+	header("Content-Disposition: attachment; filename=\"$filename\""); 
+	header("Content-Type: application/vnd.ms-excel");
+
+	// Write data to file
+	$flag = false;
+	while($row =mysqli_fetch_assoc($qur)) 
+	{
+    	if(!$flag) 
+		{
+      		// display field/column names as first row
+      		echo implode("\t", array_keys($row)) . "\r\n";
+      		$flag = true;
+    	}
+    echo implode("\t", array_values($row)) . "\r\n";
+  	}
 ?>
