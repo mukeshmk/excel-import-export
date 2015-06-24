9c9a39de3e9e89be906339b953c5293ccb154340
Mukesh <amukesh.mk@gmail.com>
2015-06-24 20:35:36 +0530

    SU - Update
    SU - Update as required

--- update/update.php
+++ update/update.php
@@ -46,10 +46,56 @@
     	border: 0px solid black;
 	}
 </style>
-	<table align='center'>
+	<?php 
+		if($_SESSION['SU']==TRUE)
+		{
+			echo'
+		<table align="center">
 		<tr>
 		<td>
-		<table align='left' class="table table-hover table-bordered table-condensed">
+		<table align="left" class="table table-hover table-bordered table-condensed">
+			<tr><td><input type="checkbox" name="formFields[]" value="SNo" checked></td><td>S.No</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="EmployeeID"></td><td>Employee ID</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="Location"></td><td>Location</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="Status"></td><td>Status</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="HostName"></td><td>Host Name</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="Brand"></td><td>Brand</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="SerialNo"></td><td>Serial No.</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="AssoDevSNo"></td><td>Associated Decvice Serial No.</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="Memory"></td><td>Memory</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="OS"></td><td>Operating System</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="InvoiceDetails"></td><td>Invoice Dectials</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="WarrentyExp"></td><td>Warrenty Expiry</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="ContactNo"></td><td>Contact No.</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="Remarks"></td><td>Remarks</td></tr>
+		</table>
+		</td>	
+		<td>
+		<table align="right"class="table table-hover table-bordered table-condensed">
+			<tr><td><input type="checkbox" name="formFields[]" value="SeatNo"></td><td>Seat No.</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="EmployeeName"></td><td>Employee Name</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="Floor"></td><td>Floor/Block</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="AssetTagNo"></td><td>Asset Tag No</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="AssetType"></td><td>Asset Type</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="Model"></td><td>Model</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="AssociatedDevices"></td><td>Associated Devices</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="HardDisk"></td><td>Hard Disk</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="Processor"></td><td>Processor</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="PurchasedOn"></td><td>Purchased On.</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="VendorDetails"></td><td>Vendor Details</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="DateofIssue"></td><td>Date of Issue</td></tr>
+			<tr><td><input type="checkbox" name="formFields[]" value="EMailID"></td><td>EMail ID</td></tr>
+			<tr><td><input type="checkbox" name="" value=""></td><td></td></tr>
+		</table>
+			';
+		}
+		else
+		{
+			echo'
+	<table align="center">
+		<tr>
+		<td>
+		<table align="left" class="table table-hover table-bordered table-condensed">
 			<tr><td><input type="checkbox" name="formFields[]" value="SNo" checked></td><td>S.No</td></tr>
 			<tr><td><input type="checkbox" name="formFields[]" value="EmployeeID"></td><td>Employee ID</td></tr>
 			<tr><td><input type="checkbox" name="formFields[]" value="Location"></td><td>Location</td></tr>
@@ -67,7 +113,7 @@
 		</table>
 		</td>	
 		<td>
-		<table align='right'class="table table-hover table-bordered table-condensed">
+		<table align="right"class="table table-hover table-bordered table-condensed">
 			<tr><td><input type="checkbox" name="formFields[]" value="SeatNo"></td><td>Seat No.</td></tr>
 			<tr><td><input type="checkbox" name="formFields[]" value="EmployeeName"></td><td>Employee Name</td></tr>
 			<tr><td><input type="checkbox" name="formFields[]" value="Floor"></td><td>Floor/Block</td></tr>
@@ -83,6 +129,9 @@
 			<tr><td><input type="checkbox" name="formFields[]" value="EMailID"></td><td>EMail ID</td></tr>
 			<tr><td><input type="checkbox" name="" value="" disabled></td><td></td></tr>
 		</table>
+		';
+		}
+	?>
 		</td>
 		</tr>
 	</table>
